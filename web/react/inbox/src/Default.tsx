4e3d5aa9fd2b4d7c6f5f3a8fea43c6b6122d318b
Mike Miller <mike@mikemiller.design>
2025-11-06 15:29:38 -0700

    Polish

--- web/react/inbox/src/Default.tsx
+++ web/react/inbox/src/Default.tsx
@@ -5,31 +5,25 @@ export default function App() {
 
   const courier = useCourier();
 
-  // To obtain a working JWT for authentication, you need to call Courier's issue-token endpoint.
-  // You must pass your user ID as part of the request to receive a JWT token specific to that user.
-  // Example: POST to https://api.courier.com/authorize/issue-token with `{ "user_id": "<your_user_id>" }` in the payload.
-  // For more information and detailed docs, see: https://www.courier.com/docs/reference/authorization
+  const generateCourierJwtOnYourServer = async () => {  
+    
+    // You should call an endpoint on your backend that wraps the Courier issue-token endpoint.
+    // For server sdk samples, see ../server-side
 
-  const getJWTFromBackend = async () => {  
-    // const response = await fetch('https://api.courier.com/issue-token', {
-    //   method: 'POST',
-    //   headers: {
-    //     'Authorization': `Bearer ${import.meta.env.VITE_COURIER_API_KEY}`,
-    //   },
-    // });
+    // Delay to simulate the time it takes to generate the JWT on your server.
+    await new Promise((resolve) => setTimeout(resolve, 1000));
     return import.meta.env.VITE_COURIER_JWT;
   };
 
   useEffect(() => {
-    const fetchJWTAndSignIn = async () => {
-      const jwt = await getJWTFromBackend();      
+    const signIn = async () => {
+      const jwt = await generateCourierJwtOnYourServer();
       courier.shared.signIn({
         userId: import.meta.env.VITE_COURIER_USER_ID,
-        jwt: jwt,
+        jwt,
       });
     };
-
-    fetchJWTAndSignIn();
+    signIn();
   }, []);
 
   return <CourierInbox />;
