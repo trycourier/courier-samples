079c8d138fc22a28290d0bf00b31cc239ca57db9
Mike Miller <mike@mikemiller.design>
2025-11-07 10:22:55 -0700

    Fixed env and showing local toast

--- web/react/toast/vite.config.ts
+++ web/react/toast/vite.config.ts
@@ -1,10 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  envDir: path.resolve(__dirname, '../../..'), // Point to project root for .env file
 })
 
